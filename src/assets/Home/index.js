8db61ff96f9b8e86701d421fa45594377a5589bf
cliff-crerar <cliffc@agilebridge.co.za>
2020-05-10 23:31:11 +0200

    setup app

---
+++ src/assets/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Home = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Home.propTypes = {
+
+};
+
+export default Home;
---
+++ src/components/page-one/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const PageOne = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+PageOne.propTypes = {
+
+};
+
+export default PageOne;
