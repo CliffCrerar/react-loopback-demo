025b5512fd4d3fda9ea07894b3e361ef65ab7085
cliff-crerar <cliffc@agilebridge.co.za>
2020-05-12 09:00:28 +0200

    udpated build

--- api/public/precache-manifest.3d8d6dcd3e0700129188396793e77b05.js
+++ api/public/precache-manifest.3d8d6dcd3e0700129188396793e77b05.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "270ae38fabd80da8233c698509008536",
+    "revision": "e32b69bb2bad38a2e9f8f33b8affbda0",
     "url": "/index.html"
   },
   {
-    "revision": "54db87cb213b23089ed7",
+    "revision": "19dffa22fcedbe509a25",
     "url": "/static/css/main.e3b1c7da.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f0cc83a6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "54db87cb213b23089ed7",
-    "url": "/static/js/main.5c480c9c.chunk.js"
+    "revision": "19dffa22fcedbe509a25",
+    "url": "/static/js/main.a5b54478.chunk.js"
   },
   {
     "revision": "3fd7f0dd5b7f17d4db60",
